525e1cbfb6ba0860aefe34d6f6e1381dc8f10195
Yannick fumukani <slashcs7@hotmail.com>
2021-12-20 01:09:06 -0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Yannick Fumukani on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
